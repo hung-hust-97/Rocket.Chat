7f5aa9d22c59f140d7d6709f8fcc7079b73ec67b
phuc2ach02 <phucquach1907@gmail.com>
2025-10-09 03:46:35 +0000

    create api search users, groups and add tenantId when create group

--- apps/meteor/app/api/server/index.ts
+++ apps/meteor/app/api/server/index.ts
@@ -42,6 +42,7 @@ import './v1/banners';
 import './v1/email-inbox';
 import './v1/mailer';
 import './v1/teams';
+import './v1/search';
 import './v1/voip/extensions';
 import './v1/voip/queues';
 import './v1/voip/omnichannel';
